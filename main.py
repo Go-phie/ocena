88dbcf7078531f358a3203be22c5ad173347aa5d
Diretnan Domnan <diretnandomnan@gmail.com>
2020-05-22 02:07:47 +0100

    Allow gophie preview apps from netlify

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    allow_origin_regex="https://deploy-preview-\d+--gophie\.netlify\.app",
 )
 
 
