65c84d9ed1a3f86e71743a9ef0bacd310bada9b6
Diretnan Domnan <diretnandomnan@gmail.com>
2020-09-24 21:12:10 +0100

    removed verbose model creation

--- app/utils.py
+++ app/utils.py
@@ -23,27 +23,14 @@ def keys_to_snake_case(d):
 
 def dict_to_model(movie_dict: dict):
     """ converts a movie_dict to model """
-    model = models.Movie(
-        name=movie_dict["title"],
-        engine=movie_dict["source"],
-        description=movie_dict.get("description", None),
-        size=movie_dict.get("size", None),
-        year=movie_dict.get("year", None),
-        download_link=movie_dict.get("download_link", None),
-        cover_photo_link=movie_dict.get("cover_photo_link", None),
-        referral_id = str(uuid.uuid4()),
-        quality=movie_dict.get("quality", None),
-        is_series=movie_dict.get("is_series", False),
-        s_download_link=movie_dict.get("s_download_link", None),
-        category=movie_dict.get("category", None),
-        cast=movie_dict.get("cast", None),
-        upload_date=movie_dict.get("upload_date", None),
-        subtitle_link=movie_dict.get("subtitle_link", None),
-        subtitle_links=movie_dict.get("subtitle_links", None),
-        imdb_link=movie_dict.get("imdb_link", None),
-        tags=movie_dict.get("tags", None),
-    )
-    return model
+    update = {
+        "engine": movie_dict["source"],
+        "name": movie_dict["title"],
+        "referral_id": str(uuid.uuid4()),
+    }
+    del movie_dict["index"], movie_dict["source"], movie_dict["title"]
+    movie_dict.update(update)
+    return models.Movie(**movie_dict)
 
 @lru_cache(maxsize=2000)
 def get_movies_from_remote(url: str, params: HashableParams, engine: str, db: HashableSession):
