d17ef50173e50d053de4ef2da01167bdb2f870ad
Diretnan Domnan <diretnandomnan@gmail.com>
2020-09-26 01:26:17 +0100

    clear cache if gophie is unreachable fixes #9

--- app/routers.py
+++ app/routers.py
@@ -117,6 +117,9 @@ def list_movies(engine: str = "netnaija", page: int = 1, num: int = 20, db: Hash
         "page": page
     })
     movies = utils.get_movies_from_remote(f"{settings.gophie_host}/list", params, engine, db)
+    if movies == "reload_cache":
+        utils.get_movies_from_remote.cache_clear()
+        movies = None
     if not movies:
         movies = crud.list_movies(db=db, engine=engine, page=page, num=num)
         logging.info(utils.get_movies_from_remote.cache_info())
@@ -137,6 +140,9 @@ def search_movies(engine: str = "netnaija", query: str = "hello", page: int = 1,
         "page": page
     })
     movies = utils.get_movies_from_remote(f"{settings.gophie_host}/search", params, engine, db)
+    if movies == "reload_cache":
+        utils.get_movies_from_remote.cache_clear()
+        movies = None
     if not movies:
         movies = crud.search_movies(db=db, engine=engine, query=query, page=page, num=num)
         logging.info(utils.get_movies_from_remote.cache_info())
