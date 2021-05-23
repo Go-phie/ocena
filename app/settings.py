0af44a77e25f167399fcb5fd440d7c3c0657894a
Deven96 <diretnandomnan@gmail.com>
2021-05-23 21:21:51 +0100

    Added allowed hosts

--- app/settings.py
+++ app/settings.py
@@ -6,7 +6,7 @@ from pydantic import BaseSettings
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "https://gophie.cam").split(",")
 
 class Settings(BaseSettings):
     app_name: str = "Ocena"
@@ -14,16 +14,7 @@ class Settings(BaseSettings):
     gophie_access_key: str = ""
     database_url: str = f"sqlite:///{BASE_DIR}/db.sqlite3"
     debug: bool = True
-    origins: list = [
-        "http://gophie-ocena.herokuapp.com",
-        "http://localhost:3000",
-        "https://gophie.netlify.app",
-        "https://gophie-ssr.herokuapp.com",
-        "https://gophie-statping.herokuapp.com",
-        "https://gophie.cam",
-        "https://ssr.gophie.cam",
-        "https://new.gophie.cam",
-    ]
+    origins: list = ALLOWED_HOSTS
     origins_regex = "https://deploy-preview-\d+--gophie\.netlify\.app"  # allow access from staging builds
 
 
