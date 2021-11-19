d12ebcdd593c31dbfc30c01720a2dc38b9d84213
sylvernus akubo <akubosylvernus@gmail.com>
2021-11-19 10:44:58 +0100

    User manager auth

--- app/dependencies.py
+++ app/dependencies.py
@@ -2,6 +2,7 @@ from fastapi import Depends, Request
 from fastapi_users import BaseUserManager
 from typing import Optional
 from fastapi_users.authentication import JWTAuthentication
+from app.models import get_user_db
 
 from app.models.models import UserCreate, UserDB
 # from .db import get_user_db
@@ -28,7 +29,7 @@ class UserManager(BaseUserManager[UserCreate, UserDB]):
             f"Verification requested for user {user.id}. Verification token: {token}")
 
 
-async def get_user_manager(user_db):
+async def get_user_manager(user_db=Depends(get_user_db)):
     yield UserManager(user_db)
 
 jwt_authentication = JWTAuthentication(
