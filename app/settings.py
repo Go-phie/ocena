77a69a98ee3d282578df432fac7de8d7c4d8948d
Mmadu Manasseh <mmadumanasseh@gmail.com>
2023-04-21 22:45:40 +0100

    K8s deployments

--- app/settings.py
+++ app/settings.py
@@ -6,16 +6,13 @@ from pydantic import BaseSettings
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "https://gophie.cam").split(",")
 
 class Settings(BaseSettings):
     app_name: str = "Ocena"
-    gophie_host: str = "https://deploy-gophie.herokuapp.com"
+    gophie_host: str = "https://gophie.cam"
     gophie_access_key: str = ""
     database_url: str = f"sqlite:///{BASE_DIR}/db.sqlite3"
     debug: bool = True
-    origins: list = ALLOWED_HOSTS
-    origins_regex = "https://deploy-preview-\d+--gophie\.netlify\.app"  # allow access from staging builds
 
 
 settings = Settings()
